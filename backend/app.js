d9e834015be8ea628a7b64c79aefce306b0e91f6
Divyanshi Srivastava <divyanshi25srivastava@gmail.com>
2025-07-14 02:07:26 +0100

    Add signup API with password hashing and user validation

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const userModel = require('./models/user.js');
 
-app.use(express.json()); 
+
+app.use(cookieParser());  
+app.use(express.json());
 
 //MongoDB Connect
 mongoose.connect(process.env.MONGO_URL, {
@@ -22,7 +27,30 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
+
+// POST /api/signup
+app.post('/api/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    // Check if user already exists
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: 'User already exists' });
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Save user
+    const user = await userModel.create({
+      username,
+      email,
+      password: hashedPassword 
+    });
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Signup nhi horha' });
+  }
+});
+
+
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
