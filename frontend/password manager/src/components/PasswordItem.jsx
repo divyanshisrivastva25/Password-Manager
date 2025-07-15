56ba35e0ab22393e9bf26844aa8a92bdee9aa77e
Divyanshi Srivastava <divyanshi25srivastava@gmail.com>
2025-07-16 00:05:20 +0100

    Add edit functionality for saved passwords

--- frontend/password manager/src/components/PasswordItem.jsx
+++ frontend/password manager/src/components/PasswordItem.jsx
@@ -1,29 +1,74 @@
-function PasswordItem({ pass,onDelete }) {
+import { useState, useEffect, useRef  } from "react";
+
+function PasswordItem({ pass, onDelete, onUpdate }) {
+  const [editing, setEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    title: pass.title,
+    username: pass.username,
+    password: pass.password,
+  });
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+  if (editing && inputRef.current) {
+    inputRef.current.focus();
+  }
+}, [editing]);
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    const updated = { ...formData, [name]: value };
+    setFormData(updated);
+    try {
+      await onUpdate(pass._id, updated);
+    } catch {
+      alert("Update failed");
+    }
+  };
+
   return (
     <div className="border rounded-lg p-4 shadow-md bg-white">
-      <h3 className="text-lg font-semibold">{pass.title}</h3>
-      <p>
-        <strong>Username:</strong> {pass.username}
-      </p>
-      <p>
-        <strong>Password:</strong> {pass.password}
-      </p>
-      <div style={{ marginTop: "10px" }}>
-  
+      {editing ? (
+        <>
+          <input name="title" value={formData.title} onChange={handleChange} ref={inputRef}  />
+          <input
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </>
+      ) : (
+        <>
+          <h3 className="text-lg font-semibold">{pass.title}</h3>
+          <p>
+            <strong>Username:</strong> {pass.username}
+          </p>
+          <p>
+            <strong>Password:</strong> {pass.password}
+          </p>
+        </>
+      )}
+      <br />
+      <div className="mt-2 ">
         <button
+          className="border border-none bg-[#4CAF50] text-white px-4"
+          onClick={() => setEditing(!editing)}
+        >
+          {editing ? "Done" : "Edit"}
+        </button>
+        <button
+          className="border border-none bg-[#f44336] text-white px-4 ml-2"
           onClick={() => onDelete(pass._id)}
-          style={{
-            padding: "5px 10px",
-            background: "#f44336",
-            color: "#fff",
-            border: "none",
-            borderRadius: "5px",
-          }}
         >
           Delete
         </button>
       </div>
-
     </div>
   );
 }
