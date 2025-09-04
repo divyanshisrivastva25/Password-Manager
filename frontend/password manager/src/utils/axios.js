b9539da45f347e425efb60025b658b5fdf38f023
Divyanshi Srivastava <divyanshi25srivastava@gmail.com>
2025-09-04 12:48:06 +0100

    chore: updated env variable name

--- frontend/password manager/src/utils/axios.js
+++ frontend/password manager/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",        // tumhare backend ka base URL
-  withCredentials: true,                       // Yeh line allow karti hai cookies send/receive karna — JWT token backend se frontend me aayega aur frontend se wapas jayega.
-});
+  baseURL: import.meta.env.BACKEND_URL + "/api",       //URL from env file
+  withCredentials: true,                                //cookies allow
+}); 
 
 export default instance;
